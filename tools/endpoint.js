f63b725f36fc49cd240c10a77b977dbd9d1cbde3
Quentin Laffont <qlaffont13@gmail.com>
2018-01-08 12:12:20 +0100

    Add getStore(), getFortnitePVEInfo()"

--- tools/endpoint.js
+++ tools/endpoint.js
@@ -3,6 +3,10 @@ module.exports = {
   OAUTH_TOKEN: "https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token",
   OAUTH_EXCHANGE: "https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/exchange",
   OAUTH_VERIFY: "https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/verify?includePerms=true",
+  FortnitePVEInfo: 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/world/info',
+  FortniteStore: 'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/storefront/v2/catalog',
+  FortniteStatus: "https://lightswitch-public-service-prod06.ol.epicgames.com/lightswitch/api/service/bulk/status?serviceId=Fortnite",
+  FortniteNews: "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game",
   lookup: (username) => {
     return "https://persona-public-service-prod06.ol.epicgames.com/persona/api/public/account/lookup?q=" + username;
   },
@@ -10,8 +14,6 @@ module.exports = {
     return "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/stats/accountId/" + accountId + "/bulk/window/alltime";
   },
   statsPVE: (accountId) => {
-    return "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/" + accountId + "/client/QueryProfile?profileId=profile0&rvn=-1";
-  },
-  FortniteStatus: "https://lightswitch-public-service-prod06.ol.epicgames.com/lightswitch/api/service/bulk/status?serviceId=Fortnite",
-  FortniteNews: "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game"
+    return "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/" + accountId + "/client/QueryProfile?profileId=athena&rvn=-1";
+  }
 };
